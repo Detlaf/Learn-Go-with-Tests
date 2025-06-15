92be8d2e7ba388e5977f199e98d1c3daf4529c2c
Detlaf <katya.gleh@yandex.ru>
2025-06-15 19:27:10 +0200

    10: concurrency

---
+++ 10_concurrency/check_website_test.go
@@ -0,0 +1,30 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+)
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://fudjsk.aksal"
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://fudjsk.aksal",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://fudjsk.aksal":        false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
